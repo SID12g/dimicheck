33d2521418389d7d88955769b6a35181379afbb5
SID12g <sid12g@naver.com>
2024-03-13 19:05:43 +0900

    fix: navigation state

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -3,11 +3,11 @@ import styles from "@/components/navigation/navigation.module.css";
 export default function Navigation() {
   return (
     <div className={styles.container}>
-      <div className={styles.button}>
-        <p className={styles.text}>위치</p>
+      <div className={styles.activeBtn}>
+        <p className={styles.activeText}>위치</p>
       </div>
-      <div className={styles.button}>
-        <p className={styles.text}>통계</p>
+      <div className={styles.deactivatedBtn}>
+        <p className={styles.deactivatedText}>통계</p>
       </div>
     </div>
   );
